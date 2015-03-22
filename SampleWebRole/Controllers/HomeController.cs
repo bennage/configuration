5d4bbfe2ddc5f0f2b0917bc8e897df874158e348
Christopher Bennage <christopher@bennage.com>
2015-03-22 15:36:14 -0700

    adding a web role to experiment with

---
+++ SampleWebRole/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿namespace SampleWebRole.Controllers
+{
+    using System;
+    using System.Web.Mvc;
+    using BetterConfig;
+    using Microsoft.Azure;
+
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Configuration.ValueStrategies.Add(CloudConfigurationManager.GetSetting);
+
+            var config = Configuration.For<IMyConfiguration>();
+            return View(config);
+        }
+
+        public ActionResult SetEnvVar()
+        {
+            Environment.SetEnvironmentVariable(
+                variable: "IMyConfiguration.MyConnectionString",
+                value: "set in process",
+                target: EnvironmentVariableTarget.Process);
+
+            return RedirectToAction("Index");
+        }
+    }
+
+    public interface IMyConfiguration
+    {
+        string MyConnectionString { get; }
+    }
+}
---
+++ SampleWebRole/Global.asax.cs
@@ -0,0 +1,14 @@
+﻿namespace SampleWebRole
+{
+    using System.Web;
+    using System.Web.Routing;
+    using App_Start;
+
+    public class MvcApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
