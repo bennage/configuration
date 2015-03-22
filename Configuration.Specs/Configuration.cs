37c4e4e813bd618ad84606c2102ce7d63b07a7aa
Christopher Bennage <christopher@bennage.com>
2015-03-21 22:57:56 -0700

    initial commit

---
+++ Configuration.Specs/Configuration.cs
@@ -0,0 +1,49 @@
+﻿namespace Bennage.Configuration.Specs
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Configuration;
+    using System.Dynamic;
+    using System.Linq;
+    using ImpromptuInterface;
+
+    public static class Configuration
+    {
+
+        public static IList<Func<string, string>> ValueStrategies = new List<Func<string, string>>()
+        {
+            key => Environment.GetEnvironmentVariable(key, EnvironmentVariableTarget.Process),
+            key => Environment.GetEnvironmentVariable(key, EnvironmentVariableTarget.User),
+            key => ConfigurationManager.AppSettings.Get(key)
+        }; 
+
+        private static string GetValueFor(string key)
+        {
+            foreach (var strategy in ValueStrategies)
+            {
+                var value = strategy(key);
+                if (value != null) return value;
+            }
+
+            return string.Empty;
+        }
+
+        public static T For<T>()
+        {
+            var type = typeof (T);
+            var rootName = type.Name;
+            var properties = type.GetProperties();
+
+            var expando = new ExpandoObject() as IDictionary<string, object>;
+
+            foreach (var property in properties)
+            {
+                var key = rootName + "." + property.Name;
+                var value = GetValueFor(key);
+                expando.Add(property.Name, value);
+            }
+
+            return expando.ActLike();
+        }
+    }
+}
---
+++ Configuration.Specs/When_loading_a_configuration.cs
@@ -0,0 +1,74 @@
+﻿namespace Specs
+{
+    using System;
+    using System.Configuration;
+    using Xunit;
+    using Configuration = Bennage.Configuration.Specs.Configuration;
+
+    public class When_loading_a_configuration
+    {
+        const string PropertyName = "IMyConfiguration.SqlConnectionString";
+
+        public When_loading_a_configuration()
+        {
+            SetEnvVar(null, EnvironmentVariableTarget.Process);
+            SetEnvVar(null, EnvironmentVariableTarget.User);
+            //SetEnvVar(null, EnvironmentVariableTarget.Machine); // requires elevated privileges
+        }
+
+        [Fact]
+        public void process_level_environmental_variables_are_picked_up()
+        {
+            var randomValue = Guid.NewGuid().ToString();
+
+            SetEnvVar(randomValue, EnvironmentVariableTarget.Process);
+
+            var config = Configuration.For<IMyConfiguration>();
+            Assert.Equal(randomValue, config.SqlConnectionString);
+        }
+
+        [Fact]
+        public void user_level_environmental_variables_are_picked_up()
+        {
+            var randomValue = Guid.NewGuid().ToString();
+
+            SetEnvVar(randomValue, EnvironmentVariableTarget.User);
+
+            var config = Configuration.For<IMyConfiguration>();
+            Assert.Equal(randomValue, config.SqlConnectionString);
+        }
+
+        [Fact]
+        public void process_level_environmental_take_precedence_over_user_level()
+        {
+            var randomValue = Guid.NewGuid().ToString();
+
+            SetEnvVar(randomValue, EnvironmentVariableTarget.Process);
+            SetEnvVar("not this value", EnvironmentVariableTarget.User);
+
+            var config = Configuration.For<IMyConfiguration>();
+            Assert.Equal(randomValue, config.SqlConnectionString);
+        }
+
+        [Fact]
+        public void can_load_from_config_file()
+        { 
+            var expected = ConfigurationManager.AppSettings.Get(PropertyName);
+            var config = Configuration.For<IMyConfiguration>();
+            Assert.Equal(expected, config.SqlConnectionString);
+        }
+
+        private static void SetEnvVar(string value, EnvironmentVariableTarget target)
+        {
+            Environment.SetEnvironmentVariable(
+                variable: PropertyName,
+                value: value,
+                target: target);
+        }
+
+        public interface IMyConfiguration
+        {
+            string SqlConnectionString { get; }
+        }
+    }
+}
